e514ab3b7123c162b6c5e797e27a45625ea60e36
Sebastian Klemke <3669903+packet23@users.noreply.github.com>
2021-10-31 17:39:58 +0100

    fix: Missing ]

--- 3.3/hadoop-hdfs-datanode/docker-entrypoint.d/05-chmod-domain-socket.sh
+++ 3.3/hadoop-hdfs-datanode/docker-entrypoint.d/05-chmod-domain-socket.sh
@@ -4,7 +4,7 @@ set -Eeo pipefail
 if hdfs getconf -confKey dfs.domain.socket.path; then
     readonly sock=$(hdfs getconf -confKey dfs.domain.socket.path)
     readonly sockdir=$(dirname "$sock")
-    if [ ! -z "${sockdir}" -a "${sockdir}" != "/"]; then
+    if [ ! -z "${sockdir}" -a "${sockdir}" != "/" ]; then
 	install -d -o root -g hdfs -m 775 "${sockdir}"
     fi
     if [ -e "${sock}" ]; then
