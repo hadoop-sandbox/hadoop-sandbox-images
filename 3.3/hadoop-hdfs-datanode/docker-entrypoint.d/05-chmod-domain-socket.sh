16554d383ae1f0a405143af70799b25efbd21c67
Sebastian Klemke <3669903+packet23@users.noreply.github.com>
2021-10-31 17:55:35 +0100

    fix: Only root may write to parent folder

--- 3.3/hadoop-hdfs-datanode/docker-entrypoint.d/05-chmod-domain-socket.sh
+++ 3.3/hadoop-hdfs-datanode/docker-entrypoint.d/05-chmod-domain-socket.sh
@@ -5,7 +5,7 @@ if hdfs getconf -confKey dfs.domain.socket.path; then
     readonly sock=$(hdfs getconf -confKey dfs.domain.socket.path)
     readonly sockdir=$(dirname "$sock")
     if [ ! -z "${sockdir}" -a "${sockdir}" != "/" ]; then
-	install -d -o root -g hdfs -m 775 "${sockdir}"
+	install -d -o root -g root -m 755 "${sockdir}"
     fi
     if [ -e "${sock}" ]; then
 	rm "${sock}"
